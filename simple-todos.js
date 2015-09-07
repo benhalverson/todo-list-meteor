2f958dd3d6dc34be69765d1a1ecd6978d95eac18
Ben Halverson <benhalverson33@gmail.com>
2015-09-07 01:28:49 -0700

    private methods finished

--- simple-todos.js
+++ simple-todos.js
@@ -2,7 +2,12 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isServer){
   // This code only runs on the server
   Meteor.publish("tasks", function () {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
 if (Meteor.isClient) {
@@ -44,6 +49,11 @@ if (Meteor.isClient) {
       Session.set("hideCompleted", event.target.checked);
     }
   });
+  Template.task.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
@@ -51,6 +61,9 @@ if (Meteor.isClient) {
     },
     "click .delete": function () {
       Meteor.call("deleteTask", this._id);
+    },
+    "click .toggle-private": function () {
+      Meteor.call("setPrivate", this._id, ! this.private);
     }
   });
   Accounts.ui.config({
@@ -72,9 +85,26 @@ Meteor.methods({
     });
   },
   deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // task is private, only the owner can delete it
+      throw new Meteor.Error("You can delete this");
+    }
     Tasks.remove(taskId);
   },
   setChecked: function (taskId, setChecked) {
+     var task = Tasks.findOne(taskId);
+     if (task.private && task.owner !== Meteor.userId()) {
+       throw new Meteor.Error("Not-authorized");
+     }
      Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("Not allowed");
+    }
+
+    Tasks.update(taskId, { $set: {private: setToPrivate } });
   }
 });
