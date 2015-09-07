fb7c603d428a03d2161c12024df33ee19b27ce78
Ben Halverson <benhalverson33@gmail.com>
2015-09-07 00:33:24 -0700

    setup users

--- simple-todos.js
+++ simple-todos.js
@@ -1,12 +1,16 @@
-'use strict';
-var Tasks;
 Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
       // Show newest tasks at the top
+      if(Session.get("hideCompleted")){
+        // if hide completed is checked, filter tasks
+      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+    } else {
       return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+
     }
   });
   Template.body.events({
@@ -20,11 +24,29 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
 
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
+  });
+  Template.task.events({
+    "click .toggle-checked": function () {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
